1da4745377cac67d41f1a9a04a943b67b8b10264
dera1992 <30830532+dera1992@users.noreply.github.com>
2026-01-26 21:47:42 +0000

    Add Redis locks for background jobs

---
+++ utils/redis_client.py
@@ -0,0 +1,12 @@
+import os
+from functools import lru_cache
+
+import redis
+
+
+@lru_cache
+def get_redis_client():
+    redis_url = os.getenv("REDIS_URL")
+    if not redis_url:
+        return None
+    return redis.Redis.from_url(redis_url, decode_responses=True)
